42f4cafdfe7119f2c8eed14ffcb24d486b60c694
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-08-11 01:28:01 +0900

    1

---
+++ variable.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// 2. Variable
+// let (added in ES6)
+
+{
+  let name = "haril";
+  console.log(name);
+  name = "hello";
+  console.log(name);
+}
+
+
+// string
+
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello ', + brendan;
+console.log('value: ${greeting}, type: ${typeof greeting}');
