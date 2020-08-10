423a1ea22c8befffa60cdac90d803600b1998b1d
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-08-11 04:00:15 +0900

    js study 1

--- variable.js
+++ variable.js
@@ -10,10 +10,53 @@
   console.log(name);
 }
 
+const bigInt = 1231231213123123145646412313213545231321654n;
+console.log();
 
 // string
 
-const char = 'c';
-const brendan = 'brendan';
-const greeting = 'hello ', + brendan;
-console.log('value: ${greeting}, type: ${typeof greeting}');
+const char = "c";
+const brendan = "brendan";
+const greeting = "hello " + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`; //template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x = undefined;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol("id");
+const symbol2 = Symbol("id");
+console.log(symbol1 === symbol2);
+const gSymbol1 = Symbol.for("id");
+const gSymbol2 = Symbol.for("id");
+console.log(gSymbol1 === gSymbol2); // true
+console.log(
+  `value: ${symbol1.description}, type: ${typeof symbol1.description}`
+);
+
+// 5. Dynamic typing: dynamically typed language
+let text = "hello";
+console.log(text.charAt(0)); //h
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = "7" + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = "8" / "2";
+console.log(`value: ${text}, type: ${typeof text}`);
+console.log(text.charAt(0));
